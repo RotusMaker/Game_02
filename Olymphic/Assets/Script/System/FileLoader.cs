d25bab0f2aae659c4d657516c081dc493576f9f3
Rotus Maker <RotusMaker@gmail.com>
2017-07-30 21:24:30 +0900

    메인 화면

---
+++ Olymphic/Assets/Script/System/FileLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileLoader : Singleton<FileLoader>
+{
+	protected FileLoader () {}	// Singletone use.
+
+	public string LoadTextAssetToResources(string fileName)
+	{
+		string path = string.Format("Data/{0}",fileName);
+
+		//Load texture from disk
+		TextAsset bindata= Resources.Load(path) as TextAsset;
+		Debug.Log(bindata.text);
+		return bindata.text;
+	}
+}
--- Olymphic/Assets/Script/UI/Logo_Nation_ScrollView.cs
+++ Olymphic/Assets/Script/UI/Logo_Nation_ScrollView.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Logo_Nation_ScrollView : MonoBehaviour {
 
 	public GameObject objElement;
+	public RectTransform rtContent;
+
+	void Start()
+	{
+		InitData ();
+	}
 
 	public void InitData()
 	{
@@ -12,5 +19,33 @@ public class Logo_Nation_ScrollView : MonoBehaviour {
 		/*
 		 * 국기이미지, 국가명, 국가번호, 주종목
 		*/
+		JSON_NationInfo.Instance.LoadJSON ();	// 필요한 데이터 체크
+
+		float elementHeight = objElement.GetComponent<RectTransform> ().sizeDelta.y;
+
+		int elementCnt = JSON_NationInfo.Instance.m_dicNationData.Count + 1;
+		rtContent.sizeDelta = new Vector2(rtContent.sizeDelta.x, elementHeight * elementCnt);
+
+		for (Dictionary<int,NationData>.Enumerator it = JSON_NationInfo.Instance.m_dicNationData.GetEnumerator (); it.MoveNext ();) 
+		{
+			GameObject element = GameObject.Instantiate (objElement);
+			element.transform.parent = rtContent.transform;
+			SetElement (element, it.Current.Value);
+		}
+	}
+
+	public void SetElement(GameObject obj, NationData data)
+	{
+		for (int i = 0; i < obj.transform.childCount; i++) 
+		{
+			Transform child = obj.transform.GetChild (i);
+			// 국가 이름
+			if (child.name.CompareTo ("name") == 0) 
+			{
+				child.GetComponent<Text> ().text = data.nationName;
+			}
+			// 국기
+			// 최고 종목
+		}
 	}
 }
